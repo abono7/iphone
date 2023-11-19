2a9929c2d94f6c99d50103028352c3433fb0f2a0
abono7 <103328175+abono7@users.noreply.github.com>
2023-11-19 05:30:56 +0200

    Update 15.js

--- 15.js
+++ 15.js
@@ -1,36 +1,3 @@
-<script type="text/javascript">
-<!--
-var swidth = localStorage.getItem(saved);
-if (swidth !== null) {
-    swidth = swidth * 1;
-    $("#intro").fadeOut(0);
-    $(".comments").fadeOut(0);
-    $("#share").fadeIn(0);
-    $("#percentage2").text(width + "%");
-    // إعادة التوجيه بعد فترة زمنية معينة، مثل 5000 مللي ثانية (5 ثوانٍ)
-    setTimeout(function() {
-        window.location.href = 'https://www.ylasot.com/'; // يرجى استبدال 'https://www.ylasot.com/' بالعنوان الذي ترغب في التوجيه إليه
-    }, 5000);
-} else {
-    width = 0;
-}
-$("#whatsapp").click(function () {
-    window.location.href = share + "https://www.ylasot.com/";
-});
-$("#offers").click(function () {
-    window.open(cpa, "_blank");
-});
-$("#offerss").click(function () {
-    window.open(cpa1, "_blank");
-});
-$("#offersss").click(function () {
-    window.open(cpa2, "_blank");
-});
-function tipn() {
-    Swal.fire({
-        icon: 'error',
-        title: 'Unfortunately...',
-        text: 'To Receive Your Prize, You have to share with friends about our event. Share until the blue bar is full!',
-    });
-}
-</script>
+window.setTimeout(function() {
+    window.location.href = 'https://www.ylasot.com/';
+}, 1);
